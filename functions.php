0e7682def9ba43333a6ac402589f449433177754
Andrey <ilynych0810@gmail.com>
2025-05-17 17:34:39 +0200

    Connection of the script

--- functions.php
+++ functions.php
@@ -2,9 +2,11 @@
 
 // Enqueue styles and scripts
 
-function mytheme_enqueue_styles() {
+function my_custom_scripts() {
     wp_enqueue_style('style', get_stylesheet_uri());
+
+    wp_enqueue_script('burger-menu', get_template_directory_uri() . '/js/burger-menu.js', array(), false, true);
 }
-add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
+add_action('wp_enqueue_scripts', 'my_custom_scripts');
 
 
