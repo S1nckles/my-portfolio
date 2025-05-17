4c702763252d3f344740e7ab10d18b2d621c197c
Andrey <ilynych0810@gmail.com>
2025-05-17 15:48:51 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+// Enqueue styles and scripts
+
+function mytheme_enqueue_styles() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
+
+
