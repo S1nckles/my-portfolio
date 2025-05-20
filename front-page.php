f9d19bc527dfc1be315cba020a18b843905de9a0
Andrey <ilynych0810@gmail.com>
2025-05-20 15:56:52 +0200

    Added Banner Section

--- front-page.php
+++ front-page.php
@@ -1,7 +1,20 @@
 <?php get_header();?>
     <main class="main">
         <div class="container">
-            
+            <div class="main__inner">
+                <section class="main__content">
+                    <div class="main__content__logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/avatar.png" alt="asd"></div>
+                    <h1 class="main__content__title">I do code and <br> make content <span>about it!</span></h1>
+                    <p class="main__content__text">I am a seasoned full-stack software engineer with over 
+                        8 years of professional experience, specializing in backend development. 
+                        My expertise lies in crafting robust and scalable SaaS-based 
+                        architectures on the Amazon AWS platform.</p>
+                    <div class="main__content__action">
+                        <button class="btn btn-active"> Get In Touch </button>
+                        <button class="btn"> Download CV </button>
+                    </div>
+                </section>
+            </div>
         </div>
     </main>
 <?php get_footer();?>
