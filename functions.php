e51bc18aad6ac8bb26e642303a9b07c84ee80627
Andrey <ilynych0810@gmail.com>
2025-05-27 17:09:57 +0200

    Added a new record type “Projects” to the admin panel

--- functions.php
+++ functions.php
@@ -1,3 +1,5 @@
+<?php add_theme_support('post-thumbnails'); ?>
+
 <?php
 
 // Enqueue styles and scripts
@@ -15,3 +17,26 @@ function register_my_menu() {
     register_nav_menu('header-menu', 'Menu');
 }
 add_action('init', 'register_my_menu');
+
+// Add Project Post Type
+
+function register_projects_post_type() {
+    register_post_type('project', [
+        'labels' => [
+            'name' => 'Projects',
+            'singular_name' => 'Project',
+            'add_new' => 'Add new project',
+            'add_new_item' => 'New project',
+            'edit_item' => 'Edit project',
+            'new_item' => 'New project',
+            'view_item' => 'View project',
+        ],
+        'public' => true,
+        'has_archive' => false,
+        'rewrite' => ['slug' => 'projects'],
+        'supports' => ['title', 'editor', 'thumbnail'],
+        'menu_icon' => 'dashicons-portfolio'
+    ]);
+}
+
+add_action('init', 'register_projects_post_type');
