2d88dfd08a320d297a96632db6f57c0121f1fb18
Andrey <ilynych0810@gmail.com>
2025-05-27 17:11:35 +0200

    Removing static elements in html and dynamic output of projects from the “Projects” record type

--- front-page.php
+++ front-page.php
@@ -27,26 +27,34 @@
                 <section class="projects" id="projects">
                     <h2 class="projects__title">Projects</h2>
                     <div class="projects__items">
-                        <div class="projects__item">
-                            <a href="#"><img src="<?php echo get_template_directory_uri();?>/assets/images/projects/01.jpg" alt="Project 1"></a>                            
-                            <div class="item__content">
-                                <div class="item__content__text">
-                                    <a href="#" class="item__content__discription">CLICK HERE TO VISIT</a>
-                                    <h3 class="item__content__title">HTML TUTORIAL</h3>
+                        <?php
+                            $projects = new WP_Query([
+                            'post_type' => 'project',
+                            'posts_per_page' => 2
+                            ]);
+
+                            if ($projects->have_posts()) :
+                            while ($projects->have_posts()) : $projects->the_post(); ?>
+                                <div class="projects__item">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php if (has_post_thumbnail()) {
+                                            the_post_thumbnail('full'); // або 'full', 'large'
+                                        } else {
+                                            echo '<div class="item__NotAImage"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/800px-Image_not_available.png" alt="Not Image" ></div>';
+                                        } ?>
+                                    </a>
+                                    <div class="item__content">
+                                        <div class="item__content__text">
+                                            <a href="<?php the_permalink(); ?>" class="item__content__discription">CLICK HERE TO VISIT</a>
+                                            <h3 class="item__content__title"><?php the_title(); ?></h3>
+                                        </div>
+                                        <a href="<?php the_permalink(); ?>" class="item__content__link"><img src="<?php echo get_template_directory_uri();?>/assets/images/icons/arrow-icon.svg" alt="Link"></a>
+                                    </div>
                                 </div>
-                                <a href="#" class="item__content__link"><img src="<?php echo get_template_directory_uri();?>/assets/images/icons/arrow-icon.svg" alt="Link"></a>
-                            </div>
-                        </div>
-                        <div class="projects__item">
-                            <a href="#"><img src="<?php echo get_template_directory_uri();?>/assets/images/projects/02.jpg" alt="Project 2"></a>                            
-                            <div class="item__content">
-                                <div class="item__content__text">
-                                    <a href="#" class="item__content__discription">CLICK HERE TO VISIT</a>
-                                    <h3 class="item__content__title">CSS TUTORIAL</h3>
-                                </div>
-                                <a href="#" class="item__content__link"><img src="<?php echo get_template_directory_uri();?>/assets/images/icons/arrow-icon.svg" alt="Link"></a>
-                            </div>
-                        </div>
+                            <?php endwhile;
+                            wp_reset_postdata();
+                            endif;
+                        ?>
                     </div>
                 </section>
                 <section class="experience" id="experience">
@@ -76,6 +84,7 @@
                     </div>
                 </section>
             </div>
+            
         </div>
     </main>
 <?php get_footer();?>
